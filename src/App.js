803bdd0be8aa863127ffb0fbf097db42fabf1656
Vardaan Agarwal <69509962+Varu98@users.noreply.github.com>
2022-03-09 22:58:30 +0530

    add: Solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import axios from "axios";
 
 export default function App() {
-  axios.get("/api/users/").then((Response) => console.log(Response));
+  // useState Variables
+  const [loader, setLoader] = useState(false);
+  const [data, setData] = useState([]);
+
+  // Click Handler Function
+  async function clickHandler() {
+    try {
+      setLoader(true);
+      const getData = await axios.get("/api/users");
+      const userData = await getData.data.users;
+
+      setData(userData);
+      setLoader(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="App">
       <h1> Data </h1>
-      <button onClick> Click to load data from server </button>
+      <button onClick={clickHandler}> Click to load data from server </button>
+      {(loader && <p>Loading...</p>) ||
+        data.map((users) => <p>{users.name}</p>)}
     </div>
   );
 }
